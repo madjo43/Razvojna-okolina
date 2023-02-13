d02ad6029b60a814caf41503d6b9f57f11a2a2f2
Mladen Ivanković <mladen.ivankovic87@gmail.com>
2023-02-13 18:53:14 +0100

    Show date in Html

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+const dateElement = document.querySelector("#date");
+const currentDate = new Date();
+
+dateElement.innerHTML = currentDate.toLocaleString();
+
+
+console.log(currentDate);
+
