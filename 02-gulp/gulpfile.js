be5fdb9c13e444262f6a5d9d4f699d8dde0082ba
Mladen Ivanković <mladen.ivankovic87@gmail.com>
2023-02-17 18:21:16 +0100

    commit on master

--- 02-gulp/gulpfile.js
+++ 02-gulp/gulpfile.js
@@ -4,23 +4,23 @@ const uglify = require("gulp-uglify");
 const sass = require("gulp-sass")(require("node-sass"));
 const cssnano = require("gulp-cssnano");
 
-gulp.task('js', function () {
-    return gulp.src('src/*.js')
+gulp.task("js", function () {
+    return gulp.src("src/*.js")
         .pipe(babel({ presets: ["@babel/preset-env"] }))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest("dist/"));
 });
 
-gulp.task('sass', function () {
-    return gulp.src('src/style.scss')
+gulp.task("sass", function () {
+    return gulp.src("src/style.scss")
         .pipe(sass())
         .pipe(cssnano())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest("dist/"));
 });
 
-gulp.task('watch', function () {
-    gulp.watch(['src/index.js'], gulp.series('js'));
-    gulp.watch(["src/style.scss"], gulp.series('sass'));
+gulp.task("watch", function () {
+    gulp.watch(["src/index.js"], gulp.series("js"));
+    gulp.watch(["src/style.scss"], gulp.series("sass"));
 });
 
-gulp.task('default', gulp.series('js', 'sass', 'watch'));
+gulp.task("default", gulp.series("js", "sass", "watch"));
